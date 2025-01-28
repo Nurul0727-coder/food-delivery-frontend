b3aae6dd7eaa94a5afa909739ce5a48b5d3f34fd
macbook pro <macbookpro@0107.local>
2025-01-28 17:12:25 +0800

    added some change

--- src/app/admin/_components/Category.tsx
+++ src/app/admin/_components/Category.tsx
@@ -35,11 +35,11 @@ export default function Category() {
 
   return (
   <div className="w-[1171px] h-[176px] bg-white ml-[250px] rounded-xl">
-    <h1 className="font-semibold ">Dishes category</h1>
+    <h1 className="font-semibold  w-[1123px] h-[28px] pt-[20px] pl-[20px] text-xl">Dishes category</h1>
     {categories.map((category)=> (
       <div key={category._id}> {category.categoryName}</div>
     ))}
-    <button className="bg-red-600  m-5 w-[36px] h-[36px] rounded-full text-slate-50" onClick={addCategory}>
+    <button className="bg-red-600  m-5 w-[36px] h-[36px] rounded-full text-slate-50 ml-[40px]" onClick={addCategory}>
       +
       </button>
    </div>
