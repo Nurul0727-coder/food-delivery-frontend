06d0515402cc833c35f8289529fa0487afd3bc6d
24LP6048 24LP6048 <24LP6048@G302-CC0121.local>
2025-02-03 17:30:45 +0800

    deploy

--- src/app/web/page.tsx
+++ src/app/web/page.tsx
@@ -1,20 +1,20 @@
-'use client';
-import {useState} from 'react';
-import {Foods} from '@/components/Foods';
-import {FoodCard} from '@/components/FoodCard';
-import {Button} from '@/components/ui/button';
-import {useAuthFetch} from '@/lib/useAuthFetch';
+// 'use client';
+// import {useState} from 'react';
+// import {Foods} from '@/components/Foods';
+// import {FoodCard} from '@/components/FoodCard';
+// import {Button} from '@/components/ui/button';
+// import {useAuthFetch} from '@/lib/useAuthFetch';
 
-type OrderItems = {
-    food: Foods;
-    quantity: number;
-};
-export default function Home() {
-const [orderItems, setOrderItems] = useState<OrderItems[]>([]);
-const {isLoading, data: foods} = useAuthFetch('foods');
-const onClick = (food: Foods) => {
-    onPost('food-order', {foodOrderItems, totalPrice:1000});
-};
-return <Foods/>;
-}
+// type OrderItems = {
+//     food: Foods;
+//     quantity: number;
+// };
+// export default function Home() {
+// const [orderItems, setOrderItems] = useState<OrderItems[]>([]);
+// const {isLoading, data: foods} = useAuthFetch('foods');
+// const onClick = (food: Foods) => {
+//     onPost('food-order', {foodOrderItems, totalPrice:1000});
+// };
+// return <Foods/>;
+// }
 
